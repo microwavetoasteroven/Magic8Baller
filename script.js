b9a2a35185b2192073a930acd537f790c9406c9e
microwavetoasteroven <69960049+microwavetoasteroven@users.noreply.github.com>
2021-05-11 07:31:20 +0100

    Update script.js

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ $(document).ready(function(){
   {
     $("#answer").hide();
     $("#8ball").attr("src", "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/magic8ballQuestion.png");
-    var question = prompt("What do you want to know?");
+    var question = "Lets try";
     magic8Ball.getAnswer(question);
   };
   
