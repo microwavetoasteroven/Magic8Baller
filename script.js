1018747d70bda3231be9bf1634bd21a880437996
microwavetoasteroven <69960049+microwavetoasteroven@users.noreply.github.com>
2021-05-11 07:53:05 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
   
   var magic8Ball = {};
-  magic8Ball.listofanswers = ["Coates family are the largest tax payer in UK, 2 years in a row", "Owns Stoke City", "Founded in 2000 with 12 staff, now at over 5000", "A USP is the range of in-game betting variety", "Denise is the wealthiest woman in the UK", "Denise acheived a first degree in econometrics", "Hillside Technology has over 1,300 talented technology professionals", "For the company!", "Locations include Manchester, Malta, Stoke, New Jersey", "John Coates is joint CEO", "53 million customers in 20 languages"];
+  magic8Ball.listofanswers = ["Coates family are the largest tax payer in UK, 2 years in a row", "Owns Stoke City", "Founded in 2000 with 12 staff, now at over 5000", "A USP is the range of in-game betting variety", "Denise is the wealthiest woman in the UK", "Denise acheived a first degree in econometrics", "Hillside Technology has over 1,300 talented technology professionals", "For the company!", "Locations include Manchester, Malta, Stoke, New Jersey", "John Coates is joint CEO", "53 million customers in 20 languages", "James Nightingale, Principle Infrastructure Architect at bet365, stated that Kubernetes could be a game-changer", "Recently open sourced Jingo, which is a fast JSON encoder library for golang.", "Recently implemented Varnish HTTP cache", ];
  
   magic8Ball.getAnswer = function(question)
   {
